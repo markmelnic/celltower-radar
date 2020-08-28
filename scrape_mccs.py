ec2272ee0a18225e69d72e20e328e5210a2ac97a
Mark Melnic <markhappy200@gmail.com>
2020-08-28 13:20:32 +0200

    Minor changes

--- scrape_mccs.py
+++ scrape_mccs.py
@@ -4,11 +4,12 @@ from bs4 import BeautifulSoup
 
 HEADERS = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 
-URL = 'https://www.mcc-mnc.com/'
+MCCS_URL = 'https://www.mcc-mnc.com/'
+MCCS_JSON = 'mccs.json'
 
 def scrape_mccs():
 
-    page = requests.get(URL, headers = HEADERS)
+    page = requests.get(MCCS_URL, headers = HEADERS)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     data = {}
